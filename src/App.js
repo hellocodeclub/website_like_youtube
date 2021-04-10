92ccf1e5d28b007d6ead567f446ebcd61dee71bd
Marta Rey <martarey@Martas-MBP.broadband>
2021-04-10 11:58:34 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/Navigation'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Navigation />
+        <div class="container" style={{marginTop:"100px"}}>
+            <div class="row p-2">
+      	        <div class="row p-2">
+      			    <div class="col-lg-3 col-xs-1 mx-auto pt-2">
+      				    <div class="card" style={{width: "41rem"}}>
+      				    	<img class="card-img-top" src="./player.png" alt="Card image cap" height="410px"/>
+      				    	<div class="card-body">
+      				    		<h5 class="card-title"><a href="http://www.google.com">Title here</a></h5>
+      				    		<p class="text-m"><small> Description here</small></p>
+      				    		<p class="text-muted font-italic"><small>Posted by <a href="#">Marta</a> Today</small></p>
+      				    	</div>
+      				    </div>
+      			    </div>
+      		    </div>
+            </div>
+        </div>
     </div>
   );
 }
---
+++ src/components/Navigation.js
@@ -0,0 +1,29 @@
+import React, {Component,useState, useEffect} from "react"
+
+const Navigation = (props) =>{
+    return(
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav" style={{background:'lightgrey'}}>
+            <div class="container" style={{color:"white"}}>
+                <a class="navbar-brand" href="/">
+            	  <img class="mr-3" src="./logo.png" height="40px" style={{borderRadius:"50px"}}/>
+            	  Your Streaming Site</a>
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                  Menu
+                  <i class="fas fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                  <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                      <a class="nav-link" href="/about">About</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="/contact-me/">Contact Me</a>
+                    </li>
+                  </ul>
+
+                </div>
+            </div>
+        </nav>
+    );
+}
+export default Navigation;
